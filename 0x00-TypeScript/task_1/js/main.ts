e323ccb43cec0a5d3fdcdceb6d1dee6006a7d613
Taha Elleuch <Tahaeleuch@gmail.com>
2021-09-12 19:35:36 +0100

    type script

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -6,3 +6,16 @@ interface Teacher {
   location: string,
   [key: string]: any
 }
+
+interface Directors extends Teacher {
+  numberOfReports: number,
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = function (firstName: string, lastName: string): string {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
